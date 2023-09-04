96b43b36b7efa4df9192ce7cda2cb38d6faf7d71
smartful <remi_rodrigues@hotmail.com>
2023-09-04 23:46:15 +0200

    inprovement of the CTA

--- components/cta/Cta.tsx
+++ components/cta/Cta.tsx
@@ -1,10 +1,27 @@
+"use client";
+
+import { useState } from "react";
 import styles from "./cta.module.css";
 
 const Cta = () => {
+  const [clicked, setClicked] = useState(false);
+
+  const toggle = () => {
+    setClicked(!clicked);
+  };
+
   return (
-    <a href="#" className={styles.ctaButton}>
-      Inscrivez-vous dès maintenant
-    </a>
+    <>
+      <button onClick={toggle} className={styles.ctaButton}>
+        Inscrivez-vous dès maintenant
+      </button>
+      {clicked && (
+        <h3 className={styles.ctaInfos}>
+          La récupération de courriels est en{" "}
+          <span className={styles.underline}>cours de construction</span> ...
+        </h3>
+      )}
+    </>
   );
 };
 
